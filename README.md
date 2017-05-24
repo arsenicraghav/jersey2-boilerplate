429f7ce8c10b0da03a352cdb0ea61adefb79dc80
arsenicraghav <mail.raghvendra.kumar@gmail.com>
2017-05-24 09:54:41 +0530

    working code

---
+++ README.md
@@ -0,0 +1,1 @@
+After running on server(tomcat) access at : http://localhost:8080/JerseyBoilerplate/sayhello/kumar
---
+++ src/com/arsenic/jersey/services/HelloWorldService.java
@@ -0,0 +1,16 @@
+package com.arsenic.jersey.services;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/sayhello")
+public class HelloWorldService {
+
+	@GET
+	@Path("/{name}")
+	public Response sayHello(@PathParam("name") String msg) {
+		String output = "Hello, " + msg + "!";
+		return Response.status(200).entity(output).build();
+	}
+}
